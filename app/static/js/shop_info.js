86eb3af5dc83e1af85b116ff3a7cbd5c9ce4c407
unknown <1451205160@qq.com>
2019-01-27 14:09:49 +0800

    购物车放大镜页面商品数据逻辑，本地缓存成功，只差css样式

--- app/static/js/shop_info.js
+++ app/static/js/shop_info.js
@@ -1,34 +1,57 @@
-var shopList = (function(){
+var shopList = (function () {
     var $shopBox = document.querySelector('.fdj_middle_right');
-    // var index = 0;
     
-    
+    //声明一个全局count
+    var count = 1;
     return {
         init() {
             this.getData();
             this.event();
         },
         event() {
+            
             var _this = this;
-            $shopBox.onmouseenter = function() {
-              
-                console.log(_this.buttonall);
-                _this.buttonall[0].onclick = function(){
-                    console.log('jian')
-                }
+            $shopBox.onmouseenter = function () {
                 
+                _this.buttonall[2].onclick = function () {
+                     //把数量添加到数据中
+                    var obj ={
+                        count:1,
+                        ..._this.data
+                    }
+                   _this.setData(obj);
+                  
+                    console.log('tijiao')
+                }
+                _this.buttonall[0].onclick = function () {
+                    count--;
+                    if(count < 1){
+                        count = 1;
+                        //此时按钮不能点击
+                        this.disabled = true;
+                        this.style = "cursor:not-allowed";
+                        alert('本商品默认一件起售'); 
+                    }
+                    _this.data.count = count;
+                    this.nextElementSibling.innerHTML = count;    
+                }
+                _this.buttonall[1].onclick = function () { 
+                    //减按钮可以点击
+                    this.previousElementSibling.previousElementSibling.disabled = false;
+                    this.previousElementSibling.previousElementSibling.style = "cursor:pointer";  
+                    count++;
+                    _this.data.count = count;
+                    this.previousElementSibling.innerHTML = count;
+                    console.log('jia')
+                }
             }
-            // console.log(_this.buttonall);
-            // this.buttonall[0].onclick = function(){
-            //     console.log("jian")
-            // }
 
         },
         //获取数据
         getData() {
             var _this = this;
-            sendAjax("http://localhost:8888/nockTone_weipin/server/php/trade_info.php",{
-                method:'get'
+            sendAjax("http://localhost:8888/nockTone_weipin/server/php/trade_info.php", {
+                method: 'get'
             }).then(data => {
                 _this.data = JSON.parse(data);
                 _this.insertData(_this.data);
@@ -36,13 +59,14 @@ var shopList = (function(){
         },
         //渲染数据
         insertData(data) {
-            // data = this.data;
-            // console.log(this);
-            // var _this = this;
-            console.log(data)
+        
+           
             //整个空数组，解决字符串渲染效率低的问题
             var arr = []
-            var str = `<p>OTHER MIX</p>
+            var $div = document.createElement('div');
+            var str = `
+            <div id=${data.id}>
+            <p>${data.brand}</p>
             <span>剩余：
 
             </span>
@@ -65,23 +89,40 @@ var shopList = (function(){
             <dl>
                 <dt>数量</dt>
                 <dd>
-                    <button>-</button>
+                    <button style="cursor: pointer">-</button>
                     <span>1</span>
-                    <button>+</button>
+                    <button style="cursor: pointer">+</button>
                 </dd>
             </dl>
-            <button>加入购物车</button>`
+            <button>加入购物车</button>
+            </div>
+            `
             arr.push(str);
             $shopBox.innerHTML = arr.join('');
-            
+
             this.buttonall = document.querySelectorAll('button');
             console.log(this.buttonall);
-            
-           
+
+
         },
         //设置数据
-        setData(){
-
+        setData(data) {
+            //声明一个本地缓存的变量
+            var shopList = localStorage.getItem("shopList") || '[]';
+            shopList = JSON.parse(shopList);
+            for(var i = 0;i < shopList.length;i++){
+                if(data.id == shopList[i].id){
+                    shopList[i].count += data.count;
+                    break;
+                }
+            }
+            //如果走到这里说明商品不存在
+            if(i == shopList.length){
+                shopList.push(data);
+            };
+            localStorage.shopList = JSON.stringify(shopList);
+            console.log(localStorage.shopList);
+            alert('添加购物车成功');
         }
     }
 }());
